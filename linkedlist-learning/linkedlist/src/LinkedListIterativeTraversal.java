b84cf0286f45a022948d68d696c64391e62e16a5
kadamati venkatasai <venkatasaikadamati@gmail.com>
2022-06-22 20:45:24 +0530

    fr

---
+++ linkedlist-learning/linkedlist/src/LinkedListIterativeTraversal.java
@@ -0,0 +1,15 @@
+public class LinkedListIterativeTraversal {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int x) {
+            data = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
